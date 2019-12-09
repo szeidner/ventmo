09213ea65358c5a12ebd7c9ef9eb21bd6987dd08
Steve Zeidner <steve@stevezeidner.com>
2019-12-08 19:03:45 -0500

    Update theme to be all dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:speech_recognition/speech_recognition.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // set a dark status bar for iOS and Android
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.white, // Color for Android
+      statusBarBrightness:
+          Brightness.dark // Dark == white status bar -- for IOS.
+      ));
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -9,11 +19,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ventmo',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.black,
-        accentColor: Colors.white,
-      ),
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: const Color(0xFF000000)),
       home: Home(),
     );
   }
@@ -48,12 +55,7 @@ class _HomeState extends State<Home> {
     _speechRecognition = SpeechRecognition();
 
     _speechRecognition.setAvailabilityHandler(
-      (bool result) => setState(() {
-        _isAvailable = result;
-        if (result) {
-          startListening();
-        }
-      }),
+      (bool result) => setState(() => _isAvailable = result),
     );
 
     _speechRecognition.setRecognitionStartedHandler(
@@ -83,22 +85,43 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              padding: EdgeInsets.symmetric(
-                vertical: 8.0,
-                horizontal: 12.0,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                padding: EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 12.0,
+                ),
+                child: Text(
+                  resultText,
+                  style: TextStyle(fontSize: 24.0),
+                ),
               ),
-              child: Text(
-                resultText,
-                style: TextStyle(fontSize: 24.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FloatingActionButton(
+                    child: Icon(Icons.mic),
+                    onPressed: () {
+                      if (_isAvailable && !_isListening) {
+                        // start listening
+                        _speechRecognition
+                            .listen(locale: "en_US")
+                            .then((result) => print('$result'));
+                      } else if (_isListening) {
+                        // stop
+                        _speechRecognition.stop().then(
+                              (result) => setState(() => _isListening = result),
+                        );
+                      }
+                    },
+                    backgroundColor: Colors.white,
+                  ),
+                ],
               ),
-            )
-          ],
-        ),
+            ]),
       ),
     );
   }
