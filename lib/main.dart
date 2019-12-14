d48fbd31416ad6efed3c3800979455e6bc560755
Steve Zeidner <steve@stevezeidner.com>
2019-12-14 14:21:45 -0500

    Add basic ai code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:speech_recognition/speech_recognition.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
 
 void main() {
   // set a dark status bar for iOS and Android
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
       title: 'Ventmo',
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: const Color(0xFF000000)),
+      debugShowCheckedModeBanner: false,
       home: Home(),
     );
   }
@@ -36,6 +38,9 @@ class _HomeState extends State<Home> {
   bool _isAvailable = false;
   bool _isListening = false;
 
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+
   String resultText = "";
 
   @override
@@ -48,7 +53,10 @@ class _HomeState extends State<Home> {
     if (_isAvailable && !_isListening)
       _speechRecognition
           .listen(locale: "en_US")
-          .then((result) => print('$result'));
+          .then((result) => () {
+            print('$result');
+            _handleSubmitted(result.toString());
+          });
   }
 
   void initSpeechRecognizer() {
@@ -73,9 +81,6 @@ class _HomeState extends State<Home> {
     _speechRecognition.activate().then(
           (result) => setState(() {
             _isAvailable = result;
-            if (result) {
-              startListening();
-            }
           }),
         );
   }
@@ -85,8 +90,8 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: Container(
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
@@ -102,26 +107,130 @@ class _HomeState extends State<Home> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  FloatingActionButton(
-                    child: Icon(Icons.mic),
-                    onPressed: () {
-                      if (_isAvailable && !_isListening) {
-                        // start listening
-                        _speechRecognition
-                            .listen(locale: "en_US")
-                            .then((result) => print('$result'));
-                      } else if (_isListening) {
-                        // stop
-                        _speechRecognition.stop().then(
-                              (result) => setState(() => _isListening = result),
-                        );
-                      }
-                    },
-                    backgroundColor: Colors.white,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 50.0),
+                    child: FloatingActionButton(
+                      child: Icon(Icons.mic),
+                      onPressed: () {
+                        if (_isAvailable && !_isListening) {
+                          // start listening
+                          startListening();
+                        } else if (_isListening) {
+                          // stop
+                          _speechRecognition.cancel().then(
+                                (result) =>
+                                    setState(() => _isListening = result),
+                              );
+                        }
+                      },
+                      backgroundColor: _isListening ? Colors.red : Colors.white,
+                    ),
                   ),
                 ],
               ),
             ]),
+      ),
+    );
+  }
+
+
+
+
+  void Response(query) async {
+    _textController.clear();
+    AuthGoogle authGoogle =
+    await AuthGoogle(fileJson: "assets/credentials.json")
+        .build();
+    Dialogflow dialogflow =
+    Dialogflow(authGoogle: authGoogle, language: Language.english);
+    AIResponse response = await dialogflow.detectIntent(query);
+    ChatMessage message = new ChatMessage(
+      text: response.getMessage() ??
+          new CardDialogflow(response.getListMessage()[0]).title,
+      name: "Bot",
+      type: false,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+      name: "Promise",
+      type: true,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    print('text');
+    Response(text);
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.name, this.type});
+
+  final String text;
+  final String name;
+  final bool type;
+
+  List<Widget> otherMessage(context) {
+    return <Widget>[
+      new Container(
+        margin: const EdgeInsets.only(right: 16.0),
+        child: new CircleAvatar(child: new Text('B')),
+      ),
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(this.name,
+                style: new TextStyle(fontWeight: FontWeight.bold)),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: new Text(text),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> myMessage(context) {
+    return <Widget>[
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            new Text(this.name, style: Theme.of(context).textTheme.subhead),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: new Text(text),
+            ),
+          ],
+        ),
+      ),
+      new Container(
+        margin: const EdgeInsets.only(left: 16.0),
+        child: new CircleAvatar(
+            child: new Text(
+              this.name[0],
+              style: new TextStyle(fontWeight: FontWeight.bold),
+            )),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.type ? myMessage(context) : otherMessage(context),
       ),
     );
   }
